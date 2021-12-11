a3ef66c835bfc5469a239178775a4b7e31b2278c
crbarbosa <carlos-barbosa@uergs.edu.br>
2021-12-11 16:26:43 -0300

    Instruções, Controle da ULA

--- src/data_path.vhd
+++ src/data_path.vhd
@@ -61,3 +61,91 @@ begin
   --a_addr <= end. reg
   --b_addr <= end. reg
   --c_addr <= end. reg
+
+  process (instruction)
+  begin
+    --decoded_instruction <= I_NOP;
+    a_addr <= "00";
+    b_addr <= "00";
+    c_addr <= "00";
+    mem_addr <= instruction(4 downto 0);
+
+    if ((instruction(15 downto 13)) = "101") then
+      c_addr <= instruction(5 downto 4);
+      elsif (instruction(15 downto 12) = "1000") then
+        c_addr <= instruction(6 downto 5);
+      elsif (instruction(15 downto 12) = "1001") then
+        c_addr <= instruction(3 downto 2);
+    end if ;
+
+    if (instruction(15) = '0')  then
+      case (instruction(9 downto 8)) is
+        when "01" =>
+          decoded_instruction <= I_BRANCH;
+        when "10" =>
+          decoded_instruction <= I_BZERO;
+        when "11" =>
+          decoded_instruction <= I_BNEG;
+        when others => 
+          decoded_instruction <= I_NOP;
+      end case;
+
+    elsif (instruction(15) = '1') then
+      case (instruction(14 downto 7)) is
+        when "00100010" =>  --MOVE
+          decoded_instruction <= I_MOVE;
+        when "01000010" => --ADD
+          decoded_instruction <= I_ADD;
+        when "01000100" => --SUB
+          decoded_instruction <= I_SUB;
+        when "01000110" => --AND
+          decoded_instruction <= I_AND;
+        when "01001000" => --OR
+          decoded_instruction <= I_OR;
+        when "00000010" => --LOAD
+          decoded_instruction <= I_LOAD;
+        when "00000100" => --STORE
+          decoded_instruction <= I_STORE;
+        when others =>
+          decoded_instruction <= I_HALT;
+      end case;
+    end if ;
+    
+    
+    if (instruction(14 downto 7) = "00100010") then --MOVE
+      a_addr <= instruction(1 downto 0);
+      b_addr <= instruction(1 downto 0);
+    elsif (instruction(14 downto 7) = "00000100") then  --STORE
+      a_addr <= instruction(6 downto 5);
+    else  --Outras operacoes
+      a_addr <= instruction(3 downto 2);
+      b_addr <= instruction(1 downto 0);  
+    end if;
+  end process;
+
+  
+  
+  --Controle da ULA
+  process(bus_a,bus_b,operation)
+    begin
+      data_out <= bus_a;
+      if (operation = "11") then
+        ula_out <=  bus_a and bus_b;--AND   11
+      elsif (operation = "10")  then
+        ula_out <=  bus_a - bus_b;  --SUB   10
+      elsif (operation = "01") then
+        ula_out <=  bus_a + bus_b;  --ADD   01
+      else
+        ula_out <= bus_a or bus_b;  --OR    00
+      end if;
+  end process;
+
+  process(clk)
+  begin
+    if (clk'event and clk = '1') then
+      --Banco de Registradores
+      if (write_reg_enable = '1') then
+        banco_de_reg(conv_integer(a_addr)) <= bus_a;
+        banco_de_reg(conv_integer(b_addr)) <= bus_b;
+        banco_de_reg(conv_integer(c_addr)) <= bus_c;
+      end if ;
