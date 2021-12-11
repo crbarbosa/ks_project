24f32c892cf1ce2790e9e8400925dbe2fc1c8793
crbarbosa <carlos-barbosa@uergs.edu.br>
2021-12-11 16:45:29 -0300

    Ajuste cabeçalho

--- src/control_unit.vhd
+++ src/control_unit.vhd
@@ -1,19 +1,6 @@
 ----------------------------------------------------------------------------------
--- Company: UERGS
--- Engineer: Joao Leonardo Fragoso
--- 
--- Create Date:    19:08:01 06/26/2012 
--- Design Name:    K and S modeling
--- Module Name:    control_unit - rtl 
--- Description:    RTL Code for K and S control unit
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
---          0.02 - moving to Vivado 2017.3
--- Additional Comments: 
---
+-- Trabalho K&S
+-- Alunos: Carlos Ricardo, Debora Garcia
 ----------------------------------------------------------------------------------
 library ieee;
 use ieee.std_logic_1164.all;
@@ -45,20 +32,5 @@ entity control_unit is
 end control_unit;
 
 architecture rtl of control_unit is
--- signal added to test environment... remove this
-signal counter : std_logic_vector(7 downto 0);
-begin
 
---process to test environment ... remove this
-    main: process(clk, rst_n)
-    begin
-        if (rst_n = '0') then
-            counter <= (others => '0');
-        elsif (clk'event and clk='1') then
-            counter <= counter + 1;
-        end if;
-    end process main;
-    halt <= '1' when counter = x"5f" else '0';
--- remove until here....
-end rtl;
 
