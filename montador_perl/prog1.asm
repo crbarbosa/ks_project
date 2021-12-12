fd616bccdb01daf23f3003fb2600b77bb6e92d1a
crbarbosa <carlos-barbosa@uergs.edu.br>
2021-12-12 11:13:03 -0300

    Inserido pasta de testes

---
+++ montador_perl/prog1.asm
@@ -0,0 +1,21 @@
+;start
+nop
+nop
+load r0 15
+load r1 16
+load r2 17
+load r3 18
+store 15 r3
+store 16 r2
+store 17 r1
+store 18 r0
+load r0 15
+load r1 16
+load r2 17
+load r3 18 
+halt
+mem 10
+mem 5
+mem 3
+mem 2
+;end program
